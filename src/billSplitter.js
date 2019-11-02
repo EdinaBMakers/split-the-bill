a24129071043e4b42b3e889961f4ba368816b257
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-11-02 18:57:59 +0000

    Refactor

--- src/billSplitter.js
+++ src/billSplitter.js
@@ -1,6 +1,14 @@
 'use strict';
 
 function splitBill(billAmount, tipInPc, numOfPeople) {
+  _validateBillAmount(billAmount);
+  _validateTip(tipInPc);
+  _validateNumOfPeople(numOfPeople);
+
+  return billAmount * (1 + tipInPc / 100) / numOfPeople;
+}
+
+function _validateBillAmount(billAmount) {
   if (isNaN(billAmount)) {
     throw new Error('Bill amount is not a number');
   }
@@ -8,7 +16,9 @@ function splitBill(billAmount, tipInPc, numOfPeople) {
   if (billAmount <= 0) {
     throw new Error('Bill amount must be greater than zero');
   }
+}
 
+function _validateTip(tipInPc) {
   if (isNaN(tipInPc)) {
     throw new Error('Tip is not a number');
   }
@@ -16,7 +26,9 @@ function splitBill(billAmount, tipInPc, numOfPeople) {
   if (tipInPc < 0) {
     throw new Error('Tip cannot be negative');
   }
+}
 
+function _validateNumOfPeople(numOfPeople) {
   if (isNaN(numOfPeople)) {
     throw new Error('Number of people is not a number');
   }
@@ -24,6 +36,4 @@ function splitBill(billAmount, tipInPc, numOfPeople) {
   if (numOfPeople <= 0) {
     throw new Error('Number of people must be greater than zero');
   }
-
-  return billAmount * (1 + tipInPc / 100) / numOfPeople;
 }
