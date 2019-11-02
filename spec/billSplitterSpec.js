ad0caed51150b759efcf488b01baae8246eadab8
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-11-02 18:33:23 +0000

    Validate input edge cases

--- spec/billSplitterSpec.js
+++ spec/billSplitterSpec.js
@@ -6,13 +6,43 @@ describe('#splitBill', () => {
       .toThrowError('Bill amount is not a number');
   });
 
+  it('throws error if bill amount is a negative number', () => {
+    expect(() => { splitBill(-1, 20, 2) })
+      .toThrowError('Bill amount must be greater than zero');
+  });
+
+  it('throws error if bill amount is zero', () => {
+    expect(() => { splitBill(0, 20, 2) })
+      .toThrowError('Bill amount must be greater than zero');
+  });
+
   it('throws error if tip is not a number', () => {
     expect(() => { splitBill(20, 'tip', 2) })
       .toThrowError('Tip is not a number');
   });
 
+  it('throws error if tip is a negative number', () => {
+    expect(() => { splitBill(20, -5, 2) })
+      .toThrowError('Tip must be greater than zero');
+  });
+
+  it('throws error if tip is zero', () => {
+    expect(() => { splitBill(20, 0, 2) })
+      .toThrowError('Tip must be greater than zero');
+  });
+
   it('throws error if number of peolpe is not a number', () => {
     expect(() => { splitBill(20, 20, 'numOfPeople') })
       .toThrowError('Number of people is not a number');
+  });
+
+  it('throws error if number of people is a negative number', () => {
+    expect(() => { splitBill(20, 20, -1) })
+      .toThrowError('Number of people must be greater than zero');
+  });
+
+  it('throws error if number of people is zero', () => {
+    expect(() => { splitBill(20, 20, 0) })
+      .toThrowError('Number of people must be greater than zero');
   });
 });
--- src/billSplitter.js
+++ src/billSplitter.js
@@ -5,11 +5,23 @@ function splitBill(billAmount, tipInPc, numOfPeople) {
     throw new Error('Bill amount is not a number');
   }
 
+  if (billAmount <= 0) {
+    throw new Error('Bill amount must be greater than zero');
+  }
+
   if (isNaN(tipInPc)) {
     throw new Error('Tip is not a number');
   }
 
+  if (tipInPc <= 0) {
+    throw new Error('Tip must be greater than zero');
+  }
+
   if (isNaN(numOfPeople)) {
     throw new Error('Number of people is not a number');
+  }
+
+  if (numOfPeople <= 0) {
+    throw new Error('Number of people must be greater than zero');
   }
 }
