40dbcbc73e4dd31ccb2824c6865c03746772ab65
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-11-17 11:21:01 +0000

    Test no error by default

--- cypress/integration/billSplitter.spec.js
+++ cypress/integration/billSplitter.spec.js
@@ -1,7 +1,13 @@
 describe('Bill Splitter', function() {
   it('can be loaded', function() {
-    cy.visit('/index.html')
+    cy.visit('/index.html');
 
-    cy.get('#title').should('have.html', 'TIP CALCULATOR')
+    cy.get('#title').should('have.html', 'TIP CALCULATOR');
+  });
+
+  it('does not show error by default', function() {
+    cy.visit('/index.html');
+
+    cy.get('#error').should('have.html', '');
   });
 });
